ec55fb22896c3a4565fa88bc2c318670b381b27f
KRISH SINHA <133884391+Krishsinha2807@users.noreply.github.com>
2026-05-01 15:16:42 +0530

    Rename authRoutes and userRoutes to lowercase

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const sessionMiddleware = require("./config/session");
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
+const authroutes = require("./routes/authroutes");
+const userroutes = require("./routes/userroutes");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(sessionMiddleware);
-app.use("/", authRoutes);
-app.use("/", userRoutes);
+app.use("/", authroutes);
+app.use("/", userroutes);
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
